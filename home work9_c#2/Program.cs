3eebe49c5b3b83716eea70fecd54227c1bd1b6a6
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-18 22:51:19 +0400

    Add files via upload

---
+++ home work9_c#2/Program.cs
@@ -0,0 +1,19 @@
+﻿//4) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin
+
+using System.Diagnostics.CodeAnalysis;
+
+int Number(int n, int m)
+{
+	int sum = 0;
+
+	for (int i = n; i < m; i++)
+	{
+		if (i % 2 == 1)
+ 		{
+			sum += i;	
+		}
+	}
+	return sum;
+}
+int result = Number(13, 18) ;
+Console.WriteLine(result);
